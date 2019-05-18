66c33d0282bd021cfc75edc1c8903b95bfa8d936
yashmarathe21 <43497991+yashmarathe21@users.noreply.github.com>
2019-05-18 12:21:30 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+      <p>The Sparks Foundation, Copyright &copy; 2019</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>The Sparks Foundation</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/transfer.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">The Sparks Foundation</span> <br> Credit Management <br> <br></h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="home.php"><strong>Home</strong></a></li>
+            <li><a href="users.php"><strong>Click here to view all Users</strong></a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
